cae32d0c28461ac8fb0970738c96d311c70623c8
Michel Brassard <mb6149@rit.edu>
2025-04-01 15:29:17 +0200

    add variable tools, fix brush

--- app/src/main/java/edu/rit/mb6149/smudge/DrawingCanvas.kt
+++ app/src/main/java/edu/rit/mb6149/smudge/DrawingCanvas.kt
@@ -1,9 +1,6 @@
 package edu.rit.mb6149.smudge
 
-import android.graphics.BitmapFactory
-import android.graphics.BitmapShader
 import android.graphics.BlurMaskFilter
-import android.graphics.Shader
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
@@ -17,25 +14,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlurEffect
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.PathFillType
-import androidx.compose.ui.graphics.ShaderBrush
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asAndroidPath
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
-import kotlin.random.Random
 
 @Composable
 fun DrawingCanvas() {
@@ -68,6 +53,7 @@ fun DrawingCanvas() {
                                 (last.y + change.position.y) / 2
                             )
                         }
+                        strokePaths.removeAt(strokePaths.lastIndex)
                         strokePaths.add(currentPath!!)
                         lastPoint = change.position
                     },
@@ -80,44 +66,18 @@ fun DrawingCanvas() {
     ) {
         Canvas(modifier = Modifier.fillMaxSize()) {
             strokePaths.forEach { path ->
-                path.fillType = PathFillType.NonZero
-                drawPath(
-                    path,
-                    color = Color.hsl(
-                        hue = 200f, //the color
-                        saturation = 0.7f, //how gray it is
-                        lightness = 0.5f, //white to black
-                    ),
-                    style = Stroke(
-                        width = 20f,
-                        cap = StrokeCap.Round,
-                        join = StrokeJoin.Round,
-                    ),
-                )
-                //For coloring? TODO
-//                drawPath(path, brush = Brush.sweepGradient(
-//                        0.0f to Color. Red,
-//                        0.3f to Color. Green,
-//                        1.0f to Color. Blue,
-//                        center = Offset(0.0f, 100.0f)
-//                    )
-//                )
-
-            //This is another tool!
-//                drawIntoCanvas { canvas ->
-//                    val paint = android.graphics.Paint().apply {
-//                        color = android.graphics.Color.RED
-//                        alpha = 100 // Keep it slightly transparent
-//                        strokeWidth = 40f // Maintain line width
-//                        style = android.graphics.Paint.Style.STROKE // Ensure it's a line, not a fill
-//                        strokeCap = android.graphics.Paint.Cap.ROUND // Smooth, round edges
-//                        maskFilter = BlurMaskFilter(10f, BlurMaskFilter.Blur.NORMAL) // Softer blur
-//                        isAntiAlias = true // Smooth rendering
-//                    }
-//                    canvas.nativeCanvas.drawPath(path.asAndroidPath(), paint)
-//                }
-
-
+                drawIntoCanvas { canvas ->
+                    val paint = android.graphics.Paint().apply {
+                        color = android.graphics.Color.BLACK
+                        alpha = 255 //0..255
+                        strokeWidth = 10f
+                        style = android.graphics.Paint.Style.STROKE
+                        strokeCap = android.graphics.Paint.Cap.ROUND
+                        maskFilter = BlurMaskFilter(2f, BlurMaskFilter.Blur.NORMAL) // Softer blur
+                        isAntiAlias = true // Smooth rendering
+                    }
+                    canvas.nativeCanvas.drawPath(path.asAndroidPath(), paint)
+                }
             }
         }
     }
